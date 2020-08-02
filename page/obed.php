ca0e8254edae463e8e9dd8825519c8506872a413
Milan Dunghubel <milan.dunghubel@gmail.com>
2020-08-02 06:12:00 +0200

    Test mysql

--- page/obed.php
+++ page/obed.php
@@ -42,6 +42,47 @@
   </div>
 </div>
 
+<?php
+$link = mysqli_connect("md84.wedos.net", 'a251653_sv1', 'Svatba_228', 'd251653_sv1');
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+// Attempt select query execution
+$sql = "SELECT * FROM persons";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>first_name</th>";
+                echo "<th>last_name</th>";
+                echo "<th>email</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['first_name'] . "</td>";
+                echo "<td>" . $row['last_name'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+?>
+
   <!-- START THE FEATURETTES -->
 
   <hr class="featurette-divider">
@@ -60,11 +101,11 @@
 
 <div class="row featurette">
   <div class="col-md-7 col-md-push-5">
-    <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    <h2 class="featurette-heading">Zámek Hrubá Skála</h2>
+    <p class="lead">Romantický zámek byl postaven na pískovcových skalách na místě středověkého hradu ze 14. století. Je jednou z dominant Českého ráje. Ze zámecké věže se nabízí krásný výhled na okolní krajinu.</p>
   </div>
   <div class="col-md-5 col-md-pull-7">
-    <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+    <img class="featurette-image img-responsive center-block" src="/img/tip/hruba-skala.jpg" alt="Hruba Skala">
   </div>
 </div>
 
