25424a67d2dd52af7dfa9c305b56ac878cedd887
Milan Dunghubel <milan.dunghubel@gmail.com>
2020-08-02 03:30:49 +0200

    Favicon

--- svatba.php
+++ svatba.php
@@ -5,7 +5,11 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/ico" href="/favicon.ico?v=<?php echo($fver); ?>" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest?v=<?php echo($fver); ?>">
+
         <title>Gabi a Milan</title>
         <!-- Bootstrap -->
         <link rel="stylesheet" href="/css/bootstrap.min.css" >
