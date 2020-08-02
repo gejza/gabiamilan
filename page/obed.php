da9739bcfd2a9c798f74329398e1bba842273cd8
Milan Dunghubel <milan.dunghubel@gmail.com>
2020-08-02 06:15:21 +0200

    Update sql

--- page/obed.php
+++ page/obed.php
@@ -50,25 +50,22 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 // Attempt select query execution
-$sql = "SELECT * FROM persons";
+$sql = "SELECT * FROM tips";
 if($result = mysqli_query($link, $sql)){
     if(mysqli_num_rows($result) > 0){
-        echo "<table>";
-            echo "<tr>";
-                echo "<th>id</th>";
-                echo "<th>first_name</th>";
-                echo "<th>last_name</th>";
-                echo "<th>email</th>";
-            echo "</tr>";
         while($row = mysqli_fetch_array($result)){
-            echo "<tr>";
+
+            echo ('<pre>');
+            var_dump($row);
+            echo ('</pre>');
+
+            /*echo "<tr>";
                 echo "<td>" . $row['id'] . "</td>";
                 echo "<td>" . $row['first_name'] . "</td>";
                 echo "<td>" . $row['last_name'] . "</td>";
                 echo "<td>" . $row['email'] . "</td>";
-            echo "</tr>";
+            echo "</tr>";*/
         }
-        echo "</table>";
         // Free result set
         mysqli_free_result($result);
     } else{
