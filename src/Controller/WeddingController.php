283b5ef61457ea2e413f3440ea79cd54b820ebdf
Milan Dunghubel <milan.dunghubel@gmail.com>
2020-08-17 16:07:57 +0200

    Base controller

--- src/Controller/WeddingController.php
+++ src/Controller/WeddingController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WeddingController extends AbstractController
+class WeddingController extends BaseController
 {
     /**
      * @Route("/svatba", name="wedding")
@@ -100,41 +100,22 @@ class WeddingController extends AbstractController
      */
     public function invite(string $hash)
     {
+        $this->addFlash(
+            'notice',
+            'Your changes were saved!'
+        );
+        // $this->addFlash() is equivalent to $request->getSession()->getFlashBag()->add()
+
+        return $this->redirectToRoute('wedding');
+
+
         $user = $this->getUser();
         $data = $this->gendata();
         $data['hash'] = $hash;
-        dump($user);
         $data['user2'] = $user;
         $data['user'] = array('checkin' => 1);
+        dump($data);
         return $this->render('invite.html.twig', $data);
     }
 
-    function gendata()
-    {
-        $data = array();
-        $data['controller_name'] = 'WeddingController';
-        $gitfile = __DIR__.'/../../git.version';
-        $data['gitcommit'] = file_exists($gitfile) ? file_get_contents($gitfile) : 'dev';
-        $data['fver'] = 32;
-        $data['siteurl'] = 'none';
-        $data['keyh'] = '';
-        $data['team'] = array();
-
-        $dm = array();
-        $dm[] = $this->gen_nav('sraz', 'Příjezd');
-        $dm[] = $this->gen_nav('obrad', 'Obřad');
-        $dm[] = $this->gen_nav('obed', 'Oběd');
-        $dm[] = $this->gen_nav('raut', 'Raut');
-        $dm[] = $this->gen_nav('dort', 'Dort');
-        $dm[] = $this->gen_nav('zabava', 'Zábava');
-        $data['menu'] = $dm;
-        return $data;
-    }
-
-    function gen_nav($name, $alt)
-    {
-        //$u = '/svatba/'.$name.'#nav';
-        $u = $this->generateUrl('wed_'.$name);
-        return array('name'=>$name, 'url'=>$u, 'alt'=>$alt);
-    }
 }
